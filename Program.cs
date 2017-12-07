7df9fa7e55f946ca0e9b2350ac9a4c96b6f11f3d
Gensitsky <vadim.gensitsky@gmail.com>
2017-12-07 07:37:51 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+    Создать обобщенный класс прямой на плоскости. В
+    классе предусмотреть 2 поля типа обобщенной точки —
+    точки, через которые проходит прямая. Реализовать
+    в классе:
+    1) конструктор, который принимает 2 точки;
+    2) конструктор, которые принимает 4 координаты (x и у
+    координаты для первой и второй точки);
+    3) метод ToString().
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpHomeCase8_3
+{
+    /// <summary>
+    /// Assignment in work
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
